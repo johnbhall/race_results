25086b55d12b034a145e8e66e2af284ec8a608eb
John B. Hall <jhall@customink.com>
2012-11-26 00:58:06 -0500

    add comma delimiters to finisher counts

--- controller.rb
+++ controller.rb
@@ -6,6 +6,13 @@ require 'chronic_duration'
 # ordinalize numbers: eg, convert 1 to 1st
 require 'active_support/core_ext/integer/inflections'
 
+# number_with_delimiter
+require 'action_view'
+
 before "index.html.erb" do
   @races = YAML.load_file('2012.yml').values.sort{|a, b| b['date'] <=> a['date']}
+end
+
+helpers do
+  include ActionView::Helpers::NumberHelper
 end
