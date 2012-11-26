e17dc4ec1b95338675edb5e2904ad70cfe9c66d4
John B. Hall <jhall@customink.com>
2012-11-26 00:28:54 -0500

    ordinalize place values (1 -> 1st, 2 -> 2nd)

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 
 gem "stasis"
 gem "chronic_duration"
+gem "active_support"
--- controller.rb
+++ controller.rb
@@ -3,6 +3,9 @@ require 'yaml'
 # the times from yaml are converted to seconds (fixnums). This gem allows us to display them in chrono time
 require 'chronic_duration'
 
+# ordinalize numbers: eg, convert 1 to 1st
+require 'active_support/core_ext/integer/inflections'
+
 before "index.html.erb" do
   @races = YAML.load_file('2012.yml').values.sort{|a, b| b['date'] <=> a['date']}
 end
